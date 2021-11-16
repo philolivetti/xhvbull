ffc1ed2f09f904598ad162bfc8cb2776c33df292
Phil Olivetti <phil@olivetti.com>
2021-11-16 18:41:21 +1300

    a bit of logos

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,8 @@ import Graph from "../components/graph"
 import createTable from "../utils/createTable"
 import { navigate } from "@reach/router"
 
+const buttonClasses =
+  "inline-block ml-2 mt-5 flex items-center bg-xhv-blue py-1 px-2 md:py-2 md:px-4 border border-transparent rounded-md text-xs font-medium text-white hover:bg-xhv-bluer"
 class IndexPage extends React.Component {
   constructor(props) {
     super(props)
@@ -189,12 +191,24 @@ class IndexPage extends React.Component {
             }}
           />
           <button
-            className="mt-5 mx-2 inline-block bg-xhv-blue py-2 px-4 border border-transparent rounded-md text-base font-medium text-white hover:bg-xhv-bluer"
+            className={`${buttonClasses}`}
             onClick={() => {
               this.setState({ showTable: !showTable })
             }}
           >
-            {showTable ? "Hide" : "Show"} Data
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5 mr-2"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z"
+                clipRule="evenodd"
+              />
+            </svg>
+            {showTable ? "Hide" : "Show"}
           </button>
         </div>
         {calculations.length == this.state.periods ? (
